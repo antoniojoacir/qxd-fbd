a05367cc9a8907d8125d8305fb694476208e95a4
antoniojoacir <antonjoacir@gmail.com>
2025-07-30 20:45:06 -0300

    route ticket

--- MaTrickets/app.py
+++ MaTrickets/app.py
@@ -4,6 +4,7 @@ from routes.contato import router as contatoRouter
 from routes.cliente import router as clienteRouter
 from routes.atracao import router as atracaoRouter
 from routes.evento import router as eventoRouter
+from routes.ticket import router as ticketRouter
 
 app = FastAPI(title="MaTrickets")
 
@@ -12,3 +13,4 @@ app.include_router(contatoRouter, prefix="/contatos", tags=["Contatos"])
 app.include_router(clienteRouter, prefix="/clientes", tags=["Clientes"])
 app.include_router(atracaoRouter, prefix="/atracoes", tags=["Atrações"])
 app.include_router(eventoRouter, prefix="/eventos", tags=["Eventos"])
+app.include_router(ticketRouter, prefix="/tickets", tags=["Tickets"])
