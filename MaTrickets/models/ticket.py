1a43503a28b9817b008f008061f4b5224096cd6b
Emilly Pinheiro <emillywpinheiro@gmail.com>
2025-07-30 23:21:20 +0000

    Ticket: model e route atualizados

--- MaTrickets/models/ticket.py
+++ MaTrickets/models/ticket.py
@@ -10,3 +10,22 @@ class Ticket(BaseModel):
     
     id_cliente: Cliente
     id_evento: Evento
+
+
+class TicketCreate(BaseModel):
+    id_ticket: int
+    numero: int
+    lote: str
+    
+    id_cliente: Cliente
+    id_evento: Evento
+
+class TicketUpdate(BaseModel):
+    id_ticket: Optional[int] = None
+    numero: Optional[int] = None
+    lote: Optional[str] = None
+    
+    id_cliente: Optional[int] = None
+    id_evento: Optional[int] = None
+   
+
--- MaTrickets/routes/ticket.py
+++ MaTrickets/routes/ticket.py
@@ -1,46 +1,267 @@
 from typing import List
 from fastapi import APIRouter, HTTPException
+from models.evento import Evento
 from models.ticket import Ticket
+from models.cliente import Cliente
 from env.db import db_connect
 
 router = APIRouter()
 
 
-@router.get("/list", response_model=List[Ticket])
-async def list_tickets():
+@router.get("/list", response_model=List[Evento])
+async def list_eventos():
     connection = db_connect()
     cursor = connection.cursor()
-    cursor.execute("SELECT id_ticket, numero, lote, id_cliente, id_evento FROM tickets")
-    data = cursor.fetchall()
-    cursor.close()
-    connection.close()
+    try:
+        cursor.execute(
+            """
+        SELECT 
+            t.id_ticket, t.numero, t.lote,
+            c.id_cliente, c.cpf, c.pnome, c.unome, c.data_nasc, c.genero, c.contato, c.endereco,
+            e.id_evento, e.titulo, e.data_inicio, e.data_fim, e.horario_inicio, e.horario_fim, e.endereco
+        FROM tickets AS t
+        LEFT JOIN clientes AS c ON t.id_cliente = c.id_cliente
+        LEFT JOIN eventos AS e ON t.id_evento = e.id_evento
+        """
+        )
+        data = cursor.fetchall()
+    except Exception as e:
+        raise HTTPException(status_code=400, detail={"NOK": {e}})
+    finally:
+        cursor.close()
+        connection.close()
     return [
         Ticket(
-            id_ticket=i[0], numero=i[1], lote=i[2], id_cliente=i[3], id_evento=i[4]
+            id_ticket=i[0],
+            numero=i[1],
+            lote=i[2],
+            evento=(
+                Evento(id_evento=i[3], 
+                titulo=i[4], 
+                data_inicio=i[5], 
+                data_fim=i[6], 
+                horario_inicio=i[7], 
+                horario_fim=i[8],
+                endereco=i[9],
+                )
+            ),
+            cliente=(
+                Cliente(
+                    id_cliente=i[10],
+                    cpf=i[11],
+                    pnome=i[12],
+                    unome=i[13],
+                    data_nasc=i[14],
+                    genero=i[15],
+                    contato=i[16],
+                    endereco=i[17],
+                )
+            ),
         )
         for i in data
     ]
 
 
-@router.post("/create")
-async def create_ticket(ticket: Ticket):
+@router.get("/get/{id}", response_model=Evento)
+async def get_ticket_by_id(id: int):
     connection = db_connect()
     cursor = connection.cursor()
     try:
         cursor.execute(
-            "INSERT INTO tickets (id_ticket, numero, lote, id_cliente, id_evento) VALUES (%s, %s, %s, %s, %s)",
-            (
-                ticket.id_ticket,
-                ticket.numero,
-                ticket.lote,
-                ticket.id_cliente,
-                ticket.id_evento,
+            """
+            SELECT 
+                t.id_ticket, t.numero, t.lote,
+                c.id_cliente, c.cpf, c.pnome, c.unome, c.data_nasc, c.genero, c.contato, c.endereco,
+                e.id_evento, e.titulo, e.data_inicio, e.data_fim, e.horario_inicio, e.horario_fim, e.endereco
+            FROM tickets AS t
+            LEFT JOIN clientes AS c ON t.id_cliente = c.id_cliente
+            LEFT JOIN eventos AS e ON t.id_evento = e.id_evento
+            WHERE id_ticket=%s
+            """,
+            (id,),
+        )
+        data = cursor.fetchone()
+    except Exception as e:
+        raise HTTPException(status_code=400, detail={"NOK": {e}})
+    finally:
+        cursor.close()
+        connection.close()
+    if data:
+        Ticket(
+            id_ticket=i[0],
+            numero=i[1],
+            lote=i[2],
+            evento=(
+                Evento(id_evento=i[3], 
+                titulo=i[4], 
+                data_inicio=i[5], 
+                data_fim=i[6], 
+                horario_inicio=i[7], 
+                horario_fim=i[8],
+                endereco=i[9],
+                )
+                if i[3] is not None
+                else None
+            ),
+            cliente=(
+                Cliente(
+                    id_cliente=i[10],
+                    cpf=i[11],
+                    pnome=i[12],
+                    unome=i[13],
+                    data_nasc=i[14],
+                    genero=i[15],
+                    contato=i[16],
+                    endereco=i[17],
+                )
+                if i[10] is not None
+                else None
             ),
         )
+    raise HTTPException(status_code=404, detail=f"NOK: Ticket {id} não encontrado.")
+
+
+@router.post("/create")
+async def create_ticket(ticket: TicketCreate):
+    data = {key: value for key, value in ticket if value is not None}
+    connection = db_connect()
+    cursor = connection.cursor()
+    try:
+        if "id_evento" in data:
+            cursor.execute(
+                """
+                SELECT id_evento 
+                FROM eventos
+                WHERE id_evento = %s
+                """,
+                (data["id_evento"],),
+            )
+            if not cursor.fetchone():
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Evento com ID {data['id_evento']} não encontrado.",
+                )
+
+        if "id_cliente" in data:
+            cursor.execute(
+                """
+                SELECT id_cliente 
+                FROM clientes
+                WHERE id_cliente = %s
+                """,
+                (data["id_cliente"],),
+            )
+            if not cursor.fetchone():
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Cliente com ID {data['id_cliente']} não encontrado.",
+                )
+
+        columns = ", ".join(data.keys())
+        placeholders = ", ".join(["%s"] * len(data))
+        values = tuple(data.values())
+        cursor.execute(
+            f"""
+            INSERT INTO eventos ({columns}) 
+            VALUES ({placeholders})
+            """,
+            values,
+        )
+        connection.commit()
     except Exception as e:
         connection.rollback()
         raise HTTPException(status_code=400, detail=f"NOK: {e}")
     finally:
         cursor.close()
         connection.close()
-    return "OK"
+    return {"OK": "Ticket criado com sucesso."}
+
+
+@router.patch("/update")
+async def update_ticket(id: int, evento: TicketUpdate):
+    data = {key: value for key, value in evento if value is not None}
+    if not data:
+        raise HTTPException(
+            status_code=400,
+            detail={"NOK": "Nenhum campo para atualizar foi fornecido."},
+        )
+    connection = db_connect()
+    cursor = connection.cursor()
+    try:
+        if "id_evento" in data:
+            cursor.execute(
+                """
+                SELECT id_evento 
+                FROM eventos
+                WHERE id_evento = %s
+                """,
+                (data["id_evento"],),
+            )
+            if not cursor.fetchone():
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Evento com ID {data['id_evento']} não encontrado.",
+                )
+
+        if "id_cliente" in data:
+            cursor.execute(
+                """
+                SELECT id_cliente 
+                FROM clientes
+                WHERE id_cliente = %s
+                """,
+                (data["id_cliente"],),
+            )
+            if not cursor.fetchone():
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Cliente com ID {data['id_cliente']} não encontrado.",
+                )
+        set_ = ", ".join([f"{key}=%s" for key in data.keys()])
+        values = list(data.values())
+        values.append(id)
+        cursor.execute(
+            f"""
+            UPDATE tickets
+            SET {set_}
+            WHERE id_ticket=%s
+            """,
+            tuple(values),
+        )
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+        raise HTTPException(status_code=400, detail=f"NOK: {e}")
+    finally:
+        cursor.close()
+        connection.close()
+    return {"OK": f"Ticket {id} atualizado com sucesso."}
+
+
+@router.delete("/delete/{id}")
+async def delete_ticket(id: int):
+    connection = db_connect()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(
+            """
+            SELECT id_ticket
+            FROM tickets 
+            WHERE id_ticket=%s
+            """,
+            (id,),
+        )
+        if not cursor.fetchone():
+            raise HTTPException(
+                status_code=404,
+                detail=f"Ticket {id} inexistente.",
+            )
+        
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+        raise HTTPException(status_code=400, detail=f"NOK {e}")
+    finally:
+        cursor.close()
+        connection.close()
+    return {"OK": f"Ticket {id} removido"}
